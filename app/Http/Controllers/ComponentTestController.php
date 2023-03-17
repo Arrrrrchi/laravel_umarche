77d97b81c6d5572ccbba9063fb153fdcba341e60
Arrrrrchi <esh6c4nm@gmail.com>
2023-03-17 10:35:46 +0900

    props

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,8 +8,7 @@ class ComponentTestController extends Controller
 {
     public function showComponent1 () {
         $message = 'メッセージ';
-        return view('tests.component-test1');
-        compact('message');
+        return view('tests.component-test1', compact('message'));
     }
 
     public function showComponent2 () {
