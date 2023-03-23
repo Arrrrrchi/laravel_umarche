6baa1e6b60b7bb0a2ad16ef3fd3cea7ab56f1372
Arrrrrchi <esh6c4nm@gmail.com>
2023-03-23 10:00:53 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloquant エロクアント
+use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダ
+use Carbon\Carbon;
+
 
 class OwnersController extends Controller
 {
@@ -12,14 +16,23 @@ class OwnersController extends Controller
         $this->middleware('auth:admin');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        dd('オーナー一覧です');
+        $date_now = Carbon::now()->year;
+        $date_parse = Carbon::parse(now());
+        echo $date_now . '</br>';
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
+
+        // $c_test = collect([
+        //     'name' => 'てすと'
+        // ]);
+
+        // dd($e_all, $q_get, $q_first, $c_test);
+        return view('admin.owners.index', compact('e_all', 'q_get'));
     }
 
     /**
