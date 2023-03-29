fcfed8cf76946a93adcdd9e2a5b6e4a3e7b00b47
Arrrrrchi <esh6c4nm@gmail.com>
2023-03-30 05:52:39 +0900

    フォームリクエスト

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <!-- バリデーションエラー -->
+                    <x-auth-validation-errors class="mb-4 text-center" :errors="$errors" />
+
                     <form action="{{ route('owner.shops.update', ['shop' => $shop->id ]) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="-m-2">
