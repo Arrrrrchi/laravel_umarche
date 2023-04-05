8015f14b49e8d875150715077db90699aee1e2cd
Arrrrrchi <esh6c4nm@gmail.com>
2023-04-05 16:05:01 +0900

    cart relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,8 @@ use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
+use App\Models\User;
+
 
 class Product extends Model
 {
@@ -63,5 +65,8 @@ class Product extends Model
         return $this->hasMany(Stock::class);
     }
 
+    public function users () {
+        return $this->belongsToMany(User::class, 'carts')->withPivot(['id', 'quantity']);
+    }
 
 }
